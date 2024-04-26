b77f06f0ebd6c7c97dd445d94d9490c49cd38d78
Trevor Brown <admin@stratus3d.com>
2024-04-25 21:48:38 -0400

    fixup! fixup! feat(golang-rewrite): extract git-specific logic out of plugins package and into plugins/git

--- plugins/git/git_test.go
+++ plugins/git/git_test.go
@@ -12,7 +12,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const testPluginName = "lua"
+// TODO: Switch to local repo so tests don't go over the network
+const (
+	testRepo       = "https://github.com/Stratus3D/asdf-lua"
+	testPluginName = "lua"
+)
 
 func TestPluginDefaultBranch(t *testing.T) {
 	testRepoPath, err := InstallMockPluginRepo(t.TempDir(), testPluginName)
@@ -35,6 +39,64 @@ func TestPluginDefaultBranch(t *testing.T) {
 		assert.ErrorContains(t, err, "remote not found")
 		assert.Equal(t, "", defaultBranch)
 	})
+}
+
+func TestGitPluginClone(t *testing.T) {
+	t.Run("when plugin name is valid but URL is invalid prints an error", func(t *testing.T) {
+		tempDir := t.TempDir()
+		directory := filepath.Join(tempDir, testPluginName)
+
+		plugin := NewGitPlugin(directory)
+		err := plugin.Clone("foobar")
+
+		assert.ErrorContains(t, err, "unable to clone plugin: repository not found")
+	})
+
+	t.Run("clones provided Git URL to plugin directory when URL is valid", func(t *testing.T) {
+		tempDir := t.TempDir()
+		directory := filepath.Join(tempDir, testPluginName)
+
+		plugin := NewGitPlugin(directory)
+		err := plugin.Clone(testRepo)
+
+		assert.Nil(t, err)
+
+		// Assert plugin directory contains Git repo with bin directory
+		_, err = os.ReadDir(directory + "/.git")
+		assert.Nil(t, err)
+
+		entries, err := os.ReadDir(directory + "/bin")
+		assert.Nil(t, err)
+		assert.Equal(t, 5, len(entries))
+	})
+}
+
+func TestGitPluginHead(t *testing.T) {
+	tempDir := t.TempDir()
+	directory := filepath.Join(tempDir, testPluginName)
+
+	plugin := NewGitPlugin(directory)
+
+	err := plugin.Clone(testRepo)
+	assert.Nil(t, err)
+
+	head, err := plugin.Head()
+	assert.Nil(t, err)
+	assert.NotZero(t, head)
+}
+
+func TestGitPluginRemoteURL(t *testing.T) {
+	tempDir := t.TempDir()
+	directory := filepath.Join(tempDir, testPluginName)
+
+	plugin := NewGitPlugin(directory)
+
+	err := plugin.Clone(testRepo)
+	assert.Nil(t, err)
+
+	url, err := plugin.RemoteURL()
+	assert.Nil(t, err)
+	assert.NotZero(t, url)
 }
 
 func InstallMockPluginRepo(dataDir, name string) (string, error) {
