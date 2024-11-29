241cecaf02f5be2e95c1624956a32c17d5a9239f
DeedleFake <deedlefake@users.noreply.github.com>
2024-11-29 15:59:45 -0500

    cmd/asdf: move from module root

--- cmd/asdf/main.go
+++ cmd/asdf/main.go
@@ -1,12 +1,12 @@
 // Main entrypoint for the CLI app
 package main
 
-import "github.com/asdf-vm/asdf-core-go/cmd"
+import "github.com/asdf-vm/asdf-core-go/cli"
 
 // Replaced with the real version during a typical build
 var version = "v-dev"
 
 // Placeholder for the real code
 func main() {
-	cmd.Execute(version)
+	cli.Execute(version)
 }
